4a4044153686085c4ffb9c87e8f4d46971efc7f3
Mark Cotton <tifosigoon@gmail.com>
2011-04-07 01:18:24 -0500

    moved javascript to /js/app.js and deployed as clean-6

---
+++ templates/_js/app.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+	console.log('jQuery is ready');
+	
+	$('.block-2 a').click(function(event) {
+    	event.preventDefault();
+    });
+	
+	$('#col_1').click(function() {
+        $('#col_2').fadeOut('slow');
+        $('#col_3').fadeOut('slow');
+        
+        setTimeout(function() {
+        	$('#result').load('/partial/past')
+        	}, 500);
+        
+	   });
+	   
+    $('#col_2').click(function() {
+        $('#col_1').hide('slow');
+        $('#col_3').hide('slow');
+
+        setTimeout(function() {
+        	$('#result').load('/partial/current')
+        	}, 500);    	   
+	   
+	   });
+	   
+    $('#col_3').click(function() {
+        $('#col_1').hide('slow');
+        $('#col_2').hide('slow');
+	   
+        setTimeout(function() {
+        	$('#result').load('/partial/future')
+        	}, 500);    	   
+	   
+	   });
+	   
+    
+});
