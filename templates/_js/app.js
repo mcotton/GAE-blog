40b0bbfd6c0b4d3a12460655e2f852568126c669
Mark Cotton <tifosigoon@gmail.com>
2011-04-26 20:27:18 -0500

    css chages and changed the blog header

--- templates/_js/app.js
+++ templates/_js/app.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
 	//console.log('jQuery is ready');
+	atoggle = 'forward';
+	
 	
 	$('.block-2 a').click(function(event) {
     	event.preventDefault();
+    	if(atoggle == 'back')  {
+    	   atoggle = 'forward';
+    	   location.reload();
+    	}
     });
 	
 	$('#col_1').click(function() {
         $('#col_2').fadeOut('slow');
         $('#col_3').fadeOut('slow');
+        $('#col_1 a').html('Back');
+        atoggle = 'back';
+        
         
         setTimeout(function() {
         	$('#result').load('/partial/past')
@@ -18,7 +27,10 @@ $(document).ready(function() {
     $('#col_2').click(function() {
         $('#col_1').hide('slow');
         $('#col_3').hide('slow');
-
+        $('#col_2 a').html('Back');
+        atoggle = 'back';
+        
+        
         setTimeout(function() {
         	$('#result').load('/partial/current')
         	}, 500);    	   
@@ -28,7 +40,10 @@ $(document).ready(function() {
     $('#col_3').click(function() {
         $('#col_1').hide('slow');
         $('#col_2').hide('slow');
-	   
+        $('#col_3 a').html('Back');
+        atoggle = 'back';
+        
+        
         setTimeout(function() {
         	$('#result').load('/partial/future')
         	}, 500);    	   
