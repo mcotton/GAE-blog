21e9f3358584b06c6c9ea20883d06ab99ad9605c
Mark Cotton <tifosigoon@gmail.com>
2014-08-14 11:56:14 -0500

    upgraded to python 2.7, remove gae_profiler

--- main.py
+++ main.py
@@ -14,45 +14,46 @@ import wsgiref.handlers
 #import cgi, time, datetime
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+import webapp2
 from google.appengine.api import users
 
 from usermodels import *  #I'm storing my models in usermodels.py
 
 
-class MainHandler(webapp.RequestHandler):
+class MainHandler(webapp2.RequestHandler):
   def get(self):
     render_template(self, 'templates/index.html')
-    
-    
-class PartialHandler(webapp.RequestHandler):
+
+
+class PartialHandler(webapp2.RequestHandler):
   def get(self, resource=''):
     #Did anything come in on the url /partial/:resource
-    if resource:  
+    if resource:
       posts = Blog().gql("where tag = :1 order by date desc", resource).fetch(1)
       if posts:
         for p in posts:
           self.response.out.write('<div class=\"block-3\" id=\"partial\"><p>' + p.content + '</p></div>')
       else:
-        self.response.out.write('There is nothing in the database about ' + resource)  
+        self.response.out.write('There is nothing in the database about ' + resource)
     else:
       self.response.out.write('Content not found')
-    
-    
-class DeleteHandler(webapp.RequestHandler):
+
+
+class DeleteHandler(webapp2.RequestHandler):
   def get(self, resource=''):
     #check if there is a key
     if resource:
       #check if they are allowed to delete things
-      if users.is_current_user_admin():  
+      if users.is_current_user_admin():
         b = Blog().get(resource)
         if b:
-          b.delete() 
-        
+          b.delete()
+
     self.redirect("/blog")
-    
-class BlogHandler(webapp.RequestHandler):
+
+class BlogHandler(webapp2.RequestHandler):
   def get(self, resource=''):
-  
+
     #Check to see if user is an admin, and display correct link
     admin = users.is_current_user_admin()
     if admin:
@@ -61,22 +62,22 @@ class BlogHandler(webapp.RequestHandler):
     else:
       admin_url = users.create_login_url("/blog")
       admin_url_text = 'Login'
-    
+
     #Did anything come in on the url /blog/:resource
-    if resource:  
+    if resource:
       posts = Blog().gql("where tag = :1 order by date desc", resource).fetch(30)
       if len(posts) == 0:
         posts = Blog().gql("order by date desc").fetch(50)
     else:
       posts = Blog().gql("order by date desc").fetch(30)
-    
+
     #Build the SideBar
     tags = SideBar().gql("order by title asc")
     tags_list = []
     for t in tags:
       tags_list.append(t.title)
-    
-    
+
+
     template_values = {
       'tags': list(set(tags_list)),
       'resource': resource,
@@ -85,10 +86,10 @@ class BlogHandler(webapp.RequestHandler):
       'admin_url': admin_url,
       'admin_url_text': admin_url_text
     }
-    
+
     render_template(self, 'templates/blog.html', template_values)
-    
-    
+
+
   def post(self, resource):
     if users.is_current_user_admin():
       b = Blog()
@@ -104,11 +105,11 @@ class BlogHandler(webapp.RequestHandler):
       updated.append(b)
       updated.append(s)
       db.put(updated)
-    
+
     self.redirect("/blog")
 
 
-class EditHandler(webapp.RequestHandler):
+class EditHandler(webapp2.RequestHandler):
   def get(self, resource=''):
     #Check to see if user is an admin, and display correct link
     admin = users.is_current_user_admin()
@@ -118,12 +119,12 @@ class EditHandler(webapp.RequestHandler):
     else:
       admin_url = users.create_login_url("/blog")
       admin_url_text = 'Login'
-  
+
     #reject anything that doesn't have a key
     if resource == '' or not admin:
       self.redirect("/blog")
-      
-    else: 
+
+    else:
       b = Blog().get(resource)
       if b is None:
         self.redirect('/blog')
@@ -135,9 +136,9 @@ class EditHandler(webapp.RequestHandler):
           'admin_url': admin_url,
           'admin_url_text': admin_url_text
           }
-    
+
         render_template(self, 'templates/edit.html', template_values)
-      
+
 
   def post(self, resource=''):
     #Check to see if user is an admin, and display correct link
@@ -148,11 +149,11 @@ class EditHandler(webapp.RequestHandler):
     else:
       admin_url = users.create_login_url("/blog")
       admin_url_text = 'Login'
-      
+
     #reject anything that doesn't have a key
     if resource == '':
       self.redirect("/blog")
-      
+
     b = Blog().get(resource)
     #check if it found anything
     if b and admin:
@@ -161,11 +162,11 @@ class EditHandler(webapp.RequestHandler):
       b.markdown = self.request.get("user_input")
       b.tag = self.request.get("category").lower()
       b.put()
-      
+
     self.redirect("/blog")
 
 
-class PostHandler(webapp.RequestHandler):
+class PostHandler(webapp2.RequestHandler):
   def get(self, resource=''):
     #Check to see if user is an admin, and display correct link
     admin = users.is_current_user_admin()
@@ -175,11 +176,11 @@ class PostHandler(webapp.RequestHandler):
     else:
       admin_url = users.create_login_url("/blog")
       admin_url_text = 'Login'
-  
+
     #reject anything that doesn't have a key
     if resource == '':
       self.redirect("/blog")
-    
+
     else:
       b = Blog().get(resource)
       template_values = {
@@ -189,14 +190,14 @@ class PostHandler(webapp.RequestHandler):
         'admin_url': admin_url,
         'admin_url_text': admin_url_text
         }
-    
+
       render_template(self, 'templates/post.html', template_values)
 
 
 
-class ExportHandler(webapp.RequestHandler):
+class ExportHandler(webapp2.RequestHandler):
   def get(self, resource=''):
-  
+
     #Check to see if user is an admin, and display correct link
     admin = users.is_current_user_admin()
     if admin:
@@ -205,22 +206,22 @@ class ExportHandler(webapp.RequestHandler):
     else:
       admin_url = users.create_login_url("/blog")
       admin_url_text = 'Login'
-    
+
     #Did anything come in on the url /blog/:resource
-    if resource:  
+    if resource:
       posts = Blog().gql("where tag = :1 order by date desc", resource).fetch(1000)
       if len(posts) == 0:
         posts = Blog().gql("order by date desc").fetch(1000)
     else:
       posts = Blog().gql("order by date desc").fetch(1000)
-    
+
     #Build the SideBar
     tags = SideBar().gql("order by title asc")
     tags_list = []
     for t in tags:
       tags_list.append(t.title)
-    
-    
+
+
     template_values = {
       'tags': list(set(tags_list)),
       'resource': resource,
@@ -229,36 +230,27 @@ class ExportHandler(webapp.RequestHandler):
       'admin_url': admin_url,
       'admin_url_text': admin_url_text
     }
-   
+
     self.response.headers['Content-Type'] = 'application/rss+xml'
     render_template(self, 'templates/rss.html', template_values)
 
 
-    
+
 def render_template(call_from, template_name, template_values=dict()):
   path = os.path.join(os.path.dirname(__file__), template_name)
   call_from.response.out.write(template.render(path, template_values))
 
 def isLocal():
-    return os.environ["SERVER_NAME"] in ("localhost") 
+    return os.environ["SERVER_NAME"] in ("localhost")
 
-def main():
-  application = webapp.WSGIApplication([('/', MainHandler),
-                                        ('/delete/([^/]+)?', DeleteHandler),
-                                        ('/blog', BlogHandler),
-                                        ('/blog/([^/]+)?', BlogHandler),
-                                        ('/partial/([^/]+)?', PartialHandler),
-                                        ('/edit', BlogHandler),
-                                        ('/edit/([^/]+)?', EditHandler),
-                                        ('/post', BlogHandler),
-                                        ('/post/([^/]+)?', PostHandler),
-                                        ('/export', ExportHandler)],
-                                         debug=isLocal())
-                                         
-  from gae_mini_profiler import profiler
-  application = profiler.ProfilerWSGIMiddleware(application)
-
-  wsgiref.handlers.CGIHandler().run(application)
-
-if __name__ == '__main__':
-  main()
+app = webapp2.WSGIApplication([('/', MainHandler),
+                                ('/delete/([^/]+)?', DeleteHandler),
+                                ('/blog', BlogHandler),
+                                ('/blog/([^/]+)?', BlogHandler),
+                                ('/partial/([^/]+)?', PartialHandler),
+                                ('/edit', BlogHandler),
+                                ('/edit/([^/]+)?', EditHandler),
+                                ('/post', BlogHandler),
+                                ('/post/([^/]+)?', PostHandler),
+                                ('/export', ExportHandler)],
+                                 debug=isLocal())
